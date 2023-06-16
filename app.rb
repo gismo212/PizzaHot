f27544b4282989ebd77efa32f6d789bd0fab925d
Elegard <whiteandy21321@gmail.com>
2023-06-16 19:24:45 +0300

    add orders model

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ set :database, {adapter: "sqlite3", database: "pizzahot.db"}
 class Product < ActiveRecord::Base 
 end
 
+class Order < ActiveRecord::Base 
+end
+
 get '/' do
 	@products = Product.all
 	erb :index 			
