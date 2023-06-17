0efaf4c93becc883108facffe4af36a05ed2fc12
Elegard <whiteandy21321@gmail.com>
2023-06-17 20:50:35 +0300

    replace cart text if cart is empty

--- public/c.js
+++ public/c.js
@@ -81,3 +81,8 @@ function cancel_order()
   window.localStorage.clear();
     update_orders_input();
     update_orders_button();
+
+    $('#cart').text('You cart is now empty');
+
+    return false;
+}
